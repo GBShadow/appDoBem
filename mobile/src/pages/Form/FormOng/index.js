ad6529859c08f52c750397204a86aba34ec4ae9e
GBShadow <gbs.shadow@gmail.com>
2020-04-19 17:33:42 -0300

    finish forms

--- mobile/src/pages/Form/FormOng/index.js
+++ mobile/src/pages/Form/FormOng/index.js
@@ -1,13 +1,111 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from "react";
+import logo from "../../../assets/logo.png";
 
-// import { Container } from './styles';
+import Background from "../../../components/Background";
 
-export default function FormOng() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SignLinkText,
+  SubmitButton,
+  SignLink,
+  SubmitText,
+  Logo,
+} from "./styles";
+
+export default function FormOng({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const cnpjRef = useRef();
+  const adressRef = useRef();
+  const phoneRef = useRef();
+
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [adress, setAdress] = useState("");
+  const [phone, setPhone] = useState("");
+
   return (
-    <Text>FormOng</Text>
+    <Background>
+      <Container>
+        <Logo source={logo} />
+
+        <Form>
+          <FormInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="next"
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Seu CNPJ"
+            ref={cnpjRef}
+            returnKeyType="next"
+            value={cnpj}
+            onChangeText={setCnpj}
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Seu endereço"
+            ref={adressRef}
+            returnKeyType="next"
+            value={adress}
+            onChangeText={setAdress}
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Seu telefone"
+            ref={phoneRef}
+            returnKeyType="send"
+            value={phone}
+            onChangeText={setPhone}
+          />
+        </Form>
+
+          <SubmitButton onPress={() => {}}>
+            <SubmitText>Criar conta</SubmitText>
+          </SubmitButton>
+
+        <SignLink onPress={() => navigation.navigate("LoginOng")}>
+          <SignLinkText>Já tenho conta</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
   );
 }
+
 
 FormOng.navigationOptions = {
   title: 'Cadastro de Ong',
--- mobile/src/pages/Form/FormOng/styles.js
+++ mobile/src/pages/Form/FormOng/styles.js
@@ -1,8 +1,59 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import Input from '../../../components/Input';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behaivior: 'padding',
+})`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding: 0 30px;
 `;
 
+export const Logo = styled.Image`
+  width: 100px;
+  height: 100px;
+`;
+
+export const Form = styled.View`
+  align-self: stretch;
+  margin-top: 50px;
+`;
+
+export const FormInput = styled(Input)`
+  width: 300px;
+  height: 40px;
+  padding: 10px 30px;
+  border-radius: 20px;
+  background: #fff;
+  margin-bottom: 20px;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  margin-top: 15px;
+  background: #fff;
+  width: 150px;
+  height: 40px;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+export const SignLinkText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const SubmitText = styled.Text`
+  color: #07224A;
+  font-size: 16px;
+  font-weight: bold;
+`;
